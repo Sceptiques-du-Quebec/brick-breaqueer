25ec922521e73040e86b9e75295172ef0d4b15a3
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-04-30 04:26:13 -0400

    Refactor

--- dist/styles/brickbreaker.core.min.css
+++ dist/styles/brickbreaker.core.min.css
@@ -7,7 +7,7 @@
     ██████╔╝╚██████╔╝╚██████╔╝   ██║   ███████╗╚██████╔╝██╔╝ ██╗
     ╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
 
-    Généré: Jeudi le 30 avril 2026 à 3 h 04
+    Généré: Jeudi le 30 avril 2026 à 4 h 26
     Auteur: Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
     Github: https://github.com/Sceptiques-du-Quebec/brick-breaqueer
 
