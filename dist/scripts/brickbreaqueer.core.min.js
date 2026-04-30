f32349fe5d8cebb0962fa8652c6aaba9d6505e96
Maxime Larrivée-Roy <zmotrin@gmail.com>
2026-04-30 17:13:49 -0400

    Ajout du changelog

--- dist/scripts/brickbreaqueer.core.min.js
+++ dist/scripts/brickbreaqueer.core.min.js
@@ -7,7 +7,7 @@
     ██████╔╝╚██████╔╝╚██████╔╝   ██║   ███████╗╚██████╔╝██╔╝ ██╗
     ╚═════╝  ╚═════╝  ╚═════╝    ╚═╝   ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
 
-    Généré: Jeudi le 30 avril 2026 à 16 h 48
+    Généré: Jeudi le 30 avril 2026 à 17 h 13
     Auteur: Maxime Larrivée-Roy <mlarriveeroy@gmail.com>
     Github: https://github.com/Sceptiques-du-Quebec/brick-breaqueer
 
